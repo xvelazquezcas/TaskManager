73fccd9a9f943a76506f169afb54c6c9831320bd
Dulce Labra <dulce.labra@repobox.com.mx>
2026-06-14 18:33:23 -0700

    Diseño del login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+ {
+    path: '', redirectTo: 'login', pathMatch: 'full'
+  },
+
+  {
+    path: 'login', component: Login
+  }
+
+];
---
+++ src/app/login/login.ts
@@ -0,0 +1,50 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+
+  loginForm: FormGroup;
+  mensaje = '';
+
+  constructor(private fb: FormBuilder) {
+
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      remember: [false]
+    });
+
+  }
+
+  iniciarSesion() {
+
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    const email = this.loginForm.value.email;
+    const password = this.loginForm.value.password;
+
+    if (
+      email === 'admin@gmail.com' &&
+      password === '123456'
+    ) {
+      this.mensaje = ' Inicio de sesión correcto';
+    } else {
+      this.mensaje = ' Correo o contraseña incorrectos';
+    }
+  }
+
+  loginGoogle() {
+    alert('Login con Google próximamente');
+  }
+
+}
